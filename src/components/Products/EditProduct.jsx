b254bae41d754c253bf65c4401a331bf459dc8cf
فهد وسام عبدالرضا شندي <fhdwsambdalrdashndy@alMacBook-Air-alkhas-bfhd.local>
2025-10-06 04:18:32 -0700

    edit

--- src/components/Products/EditProduct.jsx
+++ src/components/Products/EditProduct.jsx
@@ -329,7 +329,7 @@ const EditProduct = () => {
                             <input
                                 type="number"
                                 name="price"
-                                value={product.price}
+                                value={product.originalPrice}
                                 onChange={handleChange}
                                 className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition-colors"
                                 required
